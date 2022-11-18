11fdd0a62f22babe02641ada39adb14d7854d9f6
abdessalam <abdessalam.ait.999@gmail.com>
2022-11-18 19:37:10 +0100

    fix gradle issues changed to 7.2.2 add menu item to drawer show navigation drawer add home activity

--- app/src/main/java/com/confidenceb/beesmartwordassistant/MainActivity.kt
+++ app/src/main/java/com/confidenceb/beesmartwordassistant/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.confidenceb.beesmartwordassistant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.confidenceb.beesmartwordassistant.databinding.ActivityHomeBinding
+import com.confidenceb.beesmartwordassistant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    binding.button2.setOnClickListener {
+        intent = Intent(this@MainActivity,HomeActivity::class.java)
+        startActivity(intent)
+    }
+
     }
 }
