edd5f82fe78363c8fc1be2287312ad37bad6ca15
Confidence-bassey <confidencebass2@gmail.com>
2022-11-26 21:06:06 +0100

    Added designs and code to the main activity

--- app/src/main/java/com/confidenceb/beesmartwordassistant/MainActivity.kt
+++ app/src/main/java/com/confidenceb/beesmartwordassistant/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.confidenceb.beesmartwordassistant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.signInbtn).setOnClickListener { view ->
+            val activityIntent = Intent(this, LoginPage::class.java)
+            startActivity(activityIntent)
+        }
+
+        findViewById<Button>(R.id.signupbtn).setOnClickListener { view ->
+            val activityIntent = Intent(this, SignUpPage::class.java)
+            startActivity(activityIntent)
+        }
     }
 }
