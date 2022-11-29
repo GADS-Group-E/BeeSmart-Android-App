e2c3bd43a9f830fbd3bdfb7f72b29fed4d9d455b
Confidence-bassey <confidencebass2@gmail.com>
2022-11-29 23:41:57 +0100

    Changed LoginPage and SignUpPage from java to kotlin

---
+++ app/src/main/java/com/confidenceb/beesmartwordassistant/LoginPage.kt
@@ -0,0 +1,12 @@
+package com.confidenceb.beesmartwordassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.confidenceb.beesmartwordassistant.R
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+}
