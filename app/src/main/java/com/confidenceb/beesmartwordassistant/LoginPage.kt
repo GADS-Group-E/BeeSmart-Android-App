97db7eb788a2d2bcc53d4de7b2d0d1c556a24103
Confidence-bassey <confidencebass2@gmail.com>
2022-11-29 23:45:38 +0100

    Implemented login button'

--- app/src/main/java/com/confidenceb/beesmartwordassistant/LoginPage.kt
+++ app/src/main/java/com/confidenceb/beesmartwordassistant/LoginPage.kt
@@ -1,12 +1,19 @@
 package com.confidenceb.beesmartwordassistant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.confidenceb.beesmartwordassistant.R
 
 class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        findViewById<Button>(R.id.login).setOnClickListener { view ->
+            val activityIntent = Intent(this, HomeActivity::class.java)
+            startActivity(activityIntent)
+        }
     }
 }
