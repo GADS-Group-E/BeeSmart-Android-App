a3109a6066d6e616d71f2f754d78c9e9285a1e83
Confidence-bassey <confidencebass2@gmail.com>
2022-12-03 20:48:55 +0100

    Added Homeactivity to the AndroidManifest file

--- app/src/main/java/com/confidenceb/beesmartwordassistant/HomeActivity.kt
+++ app/src/main/java/com/confidenceb/beesmartwordassistant/HomeActivity.kt
@@ -1,11 +1,14 @@
 package com.confidenceb.beesmartwordassistant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.confidenceb.beesmartwordassistant.databinding.ActivityHomeBinding
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
     lateinit var binding:ActivityHomeBinding
@@ -15,17 +18,22 @@ class HomeActivity : AppCompatActivity() {
         binding= ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_hamburg)
 
-            toggle= ActionBarDrawerToggle(this@HomeActivity,binding.drawerLayout,R.string.open,R.string.close)
+            toggle= ActionBarDrawerToggle(this,binding.drawerLayout,toolbar,R.string.open,R.string.close)
             binding.drawerLayout.addDrawerListener(toggle)
             toggle.syncState()
 
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
             binding.NavDrawer.setNavigationItemSelectedListener {
                 when(it.itemId){
-                    R.id.dmenu_participate->{Toast.makeText(this@HomeActivity,"participate",Toast.LENGTH_SHORT).show()}
-                    R.id.dmenu_statistics->{Toast.makeText(this@HomeActivity,"statistics",Toast.LENGTH_SHORT).show()}
-                    R.id.dmenu_leaderboard->{Toast.makeText(this@HomeActivity,"dmenu_leaderboard",Toast.LENGTH_SHORT).show()}
+                    R.id.dmenu_participate->{Toast.makeText(this,"participate",Toast.LENGTH_SHORT).show()}
+                    R.id.dmenu_statistics->{Toast.makeText(this,"statistics",Toast.LENGTH_SHORT).show()}
+                    R.id.dmenu_leaderboard->{Toast.makeText(this,"dmenu_leaderboard",Toast.LENGTH_SHORT).show()}
 
 
                     }
@@ -37,22 +45,8 @@ class HomeActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         if(toggle.onOptionsItemSelected(item)){
-//            when(item.itemId) {
-//                R.id.dmenu_participate -> {
-//                    Toast.makeText(this@HomeActivity, "participate", Toast.LENGTH_SHORT).show()
-//                }
-//                R.id.dmenu_statistics -> {
-//                    Toast.makeText(this@HomeActivity, "statistics", Toast.LENGTH_SHORT)
-//                }
-//                R.id.dmenu_leaderboard -> {
-//                    Toast.makeText(this@HomeActivity, "dmenu_leaderboard", Toast.LENGTH_SHORT)
-//                }
-//
-//            }
-
             true
         }
-
 
         return super.onOptionsItemSelected(item)
     }
